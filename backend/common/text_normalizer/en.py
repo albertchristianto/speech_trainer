c6c2081773286d65df748d556c67374547001096
Albert <albertchristianto1994@gmail.com>
2026-03-29 23:01:30 +0800

    modify: folder name

---
+++ backend/common/text_normalizer/en.py
@@ -0,0 +1,16 @@
+from transformers.models.whisper.english_normalizer import BasicTextNormalizer
+
+EN_BASIC_NORMALIZER = BasicTextNormalizer()
+
+def en_normalizer(text):
+    text = text.lower()
+    text = EN_BASIC_NORMALIZER(text)
+    text = text.strip()
+    return text
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append('.')
+    text = ' hello world!!!! '
+    print(bytes(text, 'utf-8'))
+    print(bytes(en_normalizer(text), 'utf-8'))
